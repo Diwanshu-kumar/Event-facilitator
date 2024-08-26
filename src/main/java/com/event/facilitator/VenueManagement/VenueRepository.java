c3e2dba279422b432e38b2db4c07b31bc3cab29f
Diwanshu <kumardiwanshu00@gmail.com>
2024-08-27 00:34:09 +0530

    deleted : deleted previous version files.

--- src/main/java/com/event/facilitator/VenueManagement/VenueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.event.facilitator.VenueManagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface VenueRepository extends JpaRepository<Venue, Integer> {
-        List<Venue> findVenueByProviderId(int providerId);
-}
