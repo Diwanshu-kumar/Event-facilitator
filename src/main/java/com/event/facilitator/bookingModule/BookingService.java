68ffd5097b7420ebed30f711f31597ecc8996ecd
Diwanshu <kumardiwanshu00@gmail.com>
2024-08-27 22:34:51 +0530

    deleted previous version

--- src/main/java/com/event/facilitator/bookingModule/BookingService.java
+++
@@ -1,9 +0,0 @@
-package com.event.facilitator.bookingModule;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface BookingService {
-    ResponseEntity<String > bookVenue(Booking booking);
-}
--- src/main/java/com/event/facilitator/imageUpload/FileService.java
+++
@@ -1,9 +0,0 @@
-package com.event.facilitator.imageUpload;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface FileService {
-    String uploadImage(String path,MultipartFile file) throws IOException;
-}
